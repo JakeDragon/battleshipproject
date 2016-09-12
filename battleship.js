a4146d855a575f5e808468e7e3d2a6dbb9620db1
Jake Young <cheetopuffgaming@gmail.com>
2016-09-12 11:35:08 -0700

    cool

--- battleship.js
+++ battleship.js
@@ -2,6 +2,7 @@
 var rows = 10;
 var cols = 10;
 var squareSize = 50;
+var number = 16;
 
 // gets the container element
 var gameBoardContainer = document.getElementById("gameboard");
@@ -14,7 +15,7 @@ var letterConversion = {
 	"C": 2,
 	"D": 3,
 	"E": 4,
-	"F": 5,a
+	"F": 5,
 	"G": 6,
 	"H": 7,
 	"I": 8,
@@ -80,13 +81,38 @@ var gameBoard = [
 						shipsHit += 1;
 					}
 				else {
-					$("#" + coordinates).css("background-color", "grey");
+					$("#" + coordinates).css("background-color", "white");
 				}
 
 				console.log(coordinates);
 
-				if(shipsHit == 1) {
-					gameOver = false;
+				if(shipsHit == 17) {
+					gameOver = true;
+					console.log("gameOver");
+					var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 				}
 
 				if(gameOver){
