491137c0d2569256330f882188cece654e8e635a
Jake Young <cheetopuffgaming@gmail.com>
2016-09-12 20:47:46 -0700

    submitted

--- battleship.js
+++ battleship.js
@@ -2,7 +2,7 @@
 var rows = 10;
 var cols = 10;
 var squareSize = 50;
-var number = 16;
+var number = 0;
 
 // gets the container element
 var gameBoardContainer = document.getElementById("gameboard");
@@ -91,26 +91,6 @@ var gameBoard = [
 					console.log("gameOver");
 					var modal = document.getElementById('myModal');
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
     }
 }
 				}
